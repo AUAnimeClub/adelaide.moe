3150147024028a45bebc9336a8e373c1c279020c
CactusBlue <thecactusblue@gmail.com>
2022-02-23 20:38:38 +1030

    throw up something more usable for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const LogoImage = styled.img`
+  max-width: 200px;
+  margin-bottom: 40px;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <LogoImage src="/logo512.png"/>
+        Adelaide University Anime Club
+      </div>
     </div>
   );
 }
